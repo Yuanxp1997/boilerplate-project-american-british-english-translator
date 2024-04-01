a2dadf08486b914fc77fbf0d0360916448f660aa
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-03-31 21:03:48 -0600

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,15 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  const translator = new Translator();
+  // Translate Mangoes are my favorite fruit. to British English
+  test("Translate Mangoes are my favorite fruit. to British English", (done) => {
+    const input = "Mangoes are my favorite fruit.";
+    const output = "Mangoes are my favourite fruit.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
 });

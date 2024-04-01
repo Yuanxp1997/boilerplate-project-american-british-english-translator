09cd79ac014691a07c5fc0e11f679541c5904683
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-04-01 00:14:27 -0600

    finished

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,117 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
-
+  translate(text, locale, wrap = false) {
+    let translatedText = text;
+    const openTag = '<span class="highlight">';
+    const closeTag = "</span>";
+    if (locale === "american-to-british") {
+      // loop through the americanOnly object and replace the key with the value
+      for (let key in americanOnly) {
+        const regex = new RegExp(`\\b${key}\\b`, "gi");
+        const replacement = americanOnly[key];
+        if (wrap) {
+          translatedText = translatedText.replace(
+            regex,
+            `${openTag}${replacement}${closeTag}`
+          );
+        } else {
+          translatedText = translatedText.replace(regex, replacement);
+        }
+      }
+      // loop through the americanToBritishSpelling object and replace the key with the value
+      for (let key in americanToBritishSpelling) {
+        const regex = new RegExp(`\\b${key}\\b`, "gi");
+        const replacement = americanToBritishSpelling[key];
+        if (wrap) {
+          translatedText = translatedText.replace(
+            regex,
+            `${openTag}${replacement}${closeTag}`
+          );
+        } else {
+          translatedText = translatedText.replace(regex, replacement);
+        }
+      }
+      // loop through the americanToBritishTitles object and replace the key with the value
+      for (let key in americanToBritishTitles) {
+        const regex = new RegExp(`\\b${key}.\\b`, "gi");
+        const replacement =
+          americanToBritishTitles[key].charAt(0).toUpperCase() +
+          americanToBritishTitles[key].slice(1);
+        if (wrap) {
+          translatedText = translatedText.replace(
+            regex,
+            `${openTag}${replacement}${closeTag} `
+          );
+        } else {
+          translatedText = translatedText.replace(regex, replacement + " ");
+        }
+      }
+      // replace the time format from american to british
+      const timeRegex = new RegExp(/([0-9]{1,2}):([0-9]{2})/, "g");
+      if (wrap) {
+        translatedText = translatedText.replace(
+          timeRegex,
+          `${openTag}$1.$2${closeTag}`
+        );
+      } else {
+        translatedText = translatedText.replace(timeRegex, "$1.$2");
+      }
+    } else if (locale === "british-to-american") {
+      for (let key in britishOnly) {
+        const regex = new RegExp(`(?<!-)\\b${key}\\b(?!-)`, "gi");
+        const replacement = britishOnly[key];
+        if (wrap) {
+          translatedText = translatedText.replace(
+            regex,
+            `${openTag}${replacement}${closeTag}`
+          );
+        } else {
+          translatedText = translatedText.replace(regex, replacement);
+        }
+      }
+      for (let key in americanToBritishSpelling) {
+        const regex = new RegExp(
+          `\\b${americanToBritishSpelling[key]}\\b`,
+          "gi"
+        );
+        const replacement = key;
+        if (wrap) {
+          translatedText = translatedText.replace(
+            regex,
+            `${openTag}${replacement}${closeTag}`
+          );
+        } else {
+          translatedText = translatedText.replace(regex, replacement);
+        }
+      }
+      for (let key in americanToBritishTitles) {
+        const regex = new RegExp(`\\b${americanToBritishTitles[key]}\\b`, "gi");
+        const replacement = key.charAt(0).toUpperCase() + key.slice(1);
+        if (wrap) {
+          translatedText = translatedText.replace(
+            regex,
+            `${openTag}${replacement}${closeTag}`
+          );
+        } else {
+          translatedText = translatedText.replace(regex, replacement);
+        }
+      }
+      const timeRegex = new RegExp(/([0-9]{1,2}).([0-9]{2})/, "g");
+      if (wrap) {
+        translatedText = translatedText.replace(
+          timeRegex,
+          `${openTag}$1:$2${closeTag}`
+        );
+      } else {
+        translatedText = translatedText.replace(timeRegex, "$1:$2");
+      }
+    }
+    return translatedText;
+  }
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,4 +12,204 @@ suite("Unit Tests", () => {
     assert.equal(translator.translate(input, "american-to-british"), output);
     done();
   });
+
+  //Translate I ate yogurt for breakfast. to British English
+  test("Translate I ate yogurt for breakfast. to British English", (done) => {
+    const input = "I ate yogurt for breakfast.";
+    const output = "I ate yoghurt for breakfast.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate We had a party at my friend's condo. to British English
+  test("Translate We had a party at my friend's condo. to British English", (done) => {
+    const input = "We had a party at my friend's condo.";
+    const output = "We had a party at my friend's flat.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate Can you toss this in the trashcan for me? to British English
+  test("Translate Can you toss this in the trashcan for me? to British English", (done) => {
+    const input = "Can you toss this in the trashcan for me?";
+    const output = "Can you toss this in the bin for me?";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate The parking lot was full. to British English
+  test("Translate The parking lot was full. to British English", (done) => {
+    const input = "The parking lot was full.";
+    const output = "The car park was full.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate Like a high tech Rube Goldberg machine. to British English
+  test("Translate Like a high tech Rube Goldberg machine. to British English", (done) => {
+    const input = "Like a high tech Rube Goldberg machine.";
+    const output = "Like a high tech Heath Robinson device.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate To play hooky means to skip class or work. to British English
+  test("Translate To play hooky means to skip class or work. to British English", (done) => {
+    const input = "To play hooky means to skip class or work.";
+    const output = "To bunk off means to skip class or work.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate No Mr. Bond, I expect you to die. to British English
+  test("Translate No Mr. Bond, I expect you to die. to British English", (done) => {
+    const input = "No Mr. Bond, I expect you to die.";
+    const output = "No Mr Bond, I expect you to die.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate Dr. Grosh will see you now. to British English
+  test("Translate Dr. Grosh will see you now. to British English", (done) => {
+    const input = "Dr. Grosh will see you now.";
+    const output = "Dr Grosh will see you now.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate Lunch is at 12:15 today. to British English
+  test("Translate Lunch is at 12:15 today. to British English", (done) => {
+    const input = "Lunch is at 12:15 today.";
+    const output = "Lunch is at 12.15 today.";
+    assert.equal(translator.translate(input, "american-to-british"), output);
+    done();
+  });
+
+  //Translate We watched the footie match for a while. to American English
+  test("Translate We watched the footie match for a while. to American English", (done) => {
+    const input = "We watched the footie match for a while.";
+    const output = "We watched the soccer match for a while.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate Paracetamol takes up to an hour to work. to American English
+  test("Translate Paracetamol takes up to an hour to work. to American English", (done) => {
+    const input = "Paracetamol takes up to an hour to work.";
+    const output = "Tylenol takes up to an hour to work.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate First, caramelise the onions. to American English
+  test("Translate First, caramelise the onions. to American English", (done) => {
+    const input = "First, caramelise the onions.";
+    const output = "First, caramelize the onions.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate I spent the bank holiday at the funfair. to American English
+  test("Translate I spent the bank holiday at the funfair. to American English", (done) => {
+    const input = "I spent the bank holiday at the funfair.";
+    const output = "I spent the public holiday at the carnival.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate I had a bicky then went to the chippy. to American English
+  test("Translate I had a bicky then went to the chippy. to American English", (done) => {
+    const input = "I had a bicky then went to the chippy.";
+    const output = "I had a cookie then went to the fish-and-chip shop.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate I've just got bits and bobs in my bum bag. to American English
+  test("Translate I've just got bits and bobs in my bum bag. to American English", (done) => {
+    const input = "I've just got bits and bobs in my bum bag.";
+    const output = "I've just got odds and ends in my fanny pack.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate The car boot sale at Boxted Airfield was called off. to American English
+  test("Translate The car boot sale at Boxted Airfield was called off. to American English", (done) => {
+    const input = "The car boot sale at Boxted Airfield was called off.";
+    const output = "The swap meet at Boxted Airfield was called off.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate Have you met Mrs Kalyani? to American English
+  test("Translate Have you met Mrs Kalyani? to American English", (done) => {
+    const input = "Have you met Mrs Kalyani?";
+    const output = "Have you met Mrs. Kalyani?";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate Prof Joyner of King's College, London. to American English
+  test("Translate Prof Joyner of King's College, London. to American English", (done) => {
+    const input = "Prof Joyner of King's College, London.";
+    const output = "Prof. Joyner of King's College, London.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Translate Tea time is usually around 4 or 4.30. to American English
+  test("Translate Tea time is usually around 4 or 4.30. to American English", (done) => {
+    const input = "Tea time is usually around 4 or 4.30.";
+    const output = "Tea time is usually around 4 or 4:30.";
+    assert.equal(translator.translate(input, "british-to-american"), output);
+    done();
+  });
+
+  //Highlight translation in Mangoes are my favorite fruit.
+  test("Highlight translation in Mangoes are my favorite fruit.", (done) => {
+    const input = "Mangoes are my favorite fruit.";
+    const output =
+      'Mangoes are my <span class="highlight">favourite</span> fruit.';
+    assert.equal(
+      translator.translate(input, "american-to-british", true),
+      output
+    );
+    done();
+  });
+
+  // Highlight translation in I ate yogurt for breakfast.
+  test("Highlight translation in I ate yogurt for breakfast.", (done) => {
+    const input = "I ate yogurt for breakfast.";
+    const output =
+      'I ate <span class="highlight">yoghurt</span> for breakfast.';
+    assert.equal(
+      translator.translate(input, "american-to-british", true),
+      output
+    );
+    done();
+  });
+
+  // Highlight translation in We watched the footie match for a while.
+  test("Highlight translation in We watched the footie match for a while.", (done) => {
+    const input = "We watched the footie match for a while.";
+    const output =
+      'We watched the <span class="highlight">soccer</span> match for a while.';
+    assert.equal(
+      translator.translate(input, "british-to-american", true),
+      output
+    );
+    done();
+  });
+
+  // Highlight translation in Paracetamol takes up to an hour to work.
+  test("Highlight translation in Paracetamol takes up to an hour to work.", (done) => {
+    const input = "Paracetamol takes up to an hour to work.";
+    const output =
+      '<span class="highlight">Tylenol</span> takes up to an hour to work.';
+    assert.equal(
+      translator.translate(input, "british-to-american", true),
+      output
+    );
+    done();
+  });
 });
